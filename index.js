8af510bbd748ccb0e6fd915a8e862c71fa40a83f
mahmoudwael <mxheond9@gmail.com>
2020-01-20 15:59:42 +0200

    aws pipeline

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', (req, res) => {
     res.json("Aws pipeline!");
 });
